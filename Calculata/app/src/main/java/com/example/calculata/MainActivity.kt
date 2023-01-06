6f030e5b2e019d17554ba1cf3523535df01ec81d
Peter <rawiwatpan@gmail.com>
2023-01-06 16:55:20 +0700

    setup layout and onclick litsener

---
+++ Calculata/app/src/main/java/com/example/calculata/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.calculata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        button0.setOnClickListener{
+
+        }
+        button1.setOnClickListener{
+
+        }
+        button2.setOnClickListener{
+
+        }
+        button3.setOnClickListener{
+
+        }
+        button4.setOnClickListener{
+
+        }
+        button5.setOnClickListener{
+
+        }
+        button6.setOnClickListener{
+
+        }
+        button7.setOnClickListener{
+
+        }
+        button8.setOnClickListener{
+
+        }
+        button9.setOnClickListener{
+
+        }
+        buttonPlus.setOnClickListener{
+
+        }
+        buttonMinus.setOnClickListener{
+
+        }
+        buttonMultiply.setOnClickListener{
+
+        }
+        buttonDivide.setOnClickListener{
+
+        }
+        buttonEqual.setOnClickListener{
+
+        }
+        buttonErase.setOnClickListener{
+
+        }
+
+    }
+}
